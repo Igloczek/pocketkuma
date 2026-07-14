97b709068e51d340571fb0c7ca8049190e2dd914
Bartek Igielski <igloczek@gmail.com>
2026-07-14 03:58:33 +0200

    fix: preserve browser screenshot delay

--- src/server/server.ts
+++ src/server/server.ts
@@ -865,6 +865,8 @@ let needSetup = false;
                 bean.manual_status = monitor.manual_status;
                 bean.system_service_name = monitor.system_service_name;
                 bean.expected_tls_alert = monitor.expectedTlsAlert;
+                bean.screenshot_delay = monitor.screenshot_delay;
+                bean.screenshotDelay = monitor.screenshot_delay;
 
                 // ping advanced options
                 bean.ping_numeric = monitor.ping_numeric;
